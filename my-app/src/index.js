49c1b756e0d2c385d12770010d48b7b809b2a3e0
unknown <asd.shivam98@gmail.com>
2019-06-16 10:57:38 +0530

    till 22

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './Components/search_bar';
+import VideoList from './Components/video_list';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY="AIzaSyCnv8k7XTtFFq4vZ787DI1pxHcW-x3pJXY";
 class App extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={videos:[]};
+		YTSearch({key:API_KEY,term:'surfboards'},(videos)=>{
+			this.setState({videos});//equal to this.setState({videos:videos});
+		});
+	}
+
+
+
 	render(){
 		return(
 			<div>
 			Shivam saraf
 			<SearchBar />
+			<VideoList videos={this.state.videos} />
 			</div>
 			);
 	}
