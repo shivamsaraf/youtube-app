ff33bcc4305c5924e435a4b48baf2eac93913fc7
unknown <asd.shivam98@gmail.com>
2019-07-14 15:39:05 +0530

    removed authentication part

--- my-app/login/config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports= {
-	ensureAuthenticated:function(req,res,next){
-		if(req.isAuthenticated()){
-			return next();
-		}
-		req.flash('error_msg','plz login to view this record');
-		res.redirect('/users/login');
-	}
-}
--- my-app/login/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-MongoURI:"mongodb+srv://Shivamsalil:shivam123@mavihs-mswho.mongodb.net/test?retryWrites=true&w=majority"
-}
